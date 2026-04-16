733d1e07796d40956754b5bb7515e2fa638e90fa
Mingyuan Ma <cnwhmmy@outlook.com>
2026-04-16 15:31:25 +0800

    set gpgpu-sim branch to modern-modeling

--- gpu-simulator/setup_environment.sh
+++ gpu-simulator/setup_environment.sh
@@ -66,7 +66,7 @@ export ACCELSIM_ROOT="$( cd "$( dirname "$SCRIPT_LOC" )" && pwd )"
 #   For development, we use our internal repo and the dev branch
 #       Ideally, when we release, it should be based off a GPGPU-Sim release.
 export GPGPUSIM_REPO="${GPGPUSIM_REPO:=https://github.com/reoLantern/gpgpu-sim_distribution.git}"
-export GPGPUSIM_BRANCH="${GPGPUSIM_BRANCH:=dev}"
+export GPGPUSIM_BRANCH="${GPGPUSIM_BRANCH:=modern-modeling}"
 
 # Help the user out by setting the default CUDA_INSTALL_PATH, if it is not already set
 if [ -z "$CUDA_INSTALL_PATH" ]; then
